13af690a34a4220e301a46db7ec7c325ddd3f9da
ConnorMul <mulhollandconnor@icloud.com>
2021-01-13 19:30:03 -0500

    commit tree 87922d230820e4ceca2d6629c8a9b8e4eea32f9b

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+    const [dogs, setDogs] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+    
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+            setDogs(data)
+            setIsLoaded(true)
+        })
+    }, [])
+
+    if (!isLoaded) return <p>Loading</p>;
+
+    return(
+        <img src={dogs.message} alt="A Random Dog"></img>
+    )
+}
+
+export default App
